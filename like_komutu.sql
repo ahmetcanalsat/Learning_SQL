6d48636aab54bfbcae572b449173aee9fc3c4a55
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-06 03:26:23 +0300

    How to use 'LIKE' comment
    Like komutumuzu kullanıp tablolarımızın içinde arama yapmayı öğrendik.

---
+++ like_komutu.sql
@@ -0,0 +1,4 @@
+﻿use DbUrunYonetim
+
+SELECT * FROM tblPersonal WHERE pers_Name LIKE '%A%'
+/*Yukarıdaki komutumuzla Personal tablomuzun Name sütunundaki verilerin içinde 'A' harfi geçenleri listelettik.*/ 
