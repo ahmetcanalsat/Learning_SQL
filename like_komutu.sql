b466e0be8338ab254bc6574c18d9de42d329fd1b
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-09 00:47:01 +0300

    Like_komutu yeni örnekler
    Farklı örnekler yazıp açıklayarak Like komutunun kullanımını pekiştirdik.

--- like_komutu.sql
+++ like_komutu.sql
@@ -1,4 +1,17 @@
 ﻿use DbUrunYonetim
 
-SELECT * FROM tblPersonal WHERE pers_Name LIKE '%A%'
+/*SELECT * FROM tblPersonal WHERE pers_Name LIKE '%A%'*/
 /*Yukarıdaki komutumuzla Personal tablomuzun Name sütunundaki verilerin içinde 'A' harfi geçenleri listelettik.*/ 
+
+
+/*SELECT * FROM tblPersonal WHERE pers_Name LIKE '%ES%'*/
+/*Yukarıdaki komutumuzla Personal tablomuzun içinden adında 'ES' geçen elemanları listelettik.*/
+
+/*SELECT * FROM tblPersonal WHERE pers_Department LIKE 'S%'*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan Departmanının adı 'S' ile başlayan verileri listelettik.*/
+
+/*SELECT * FROM tblPersonal WHERE pers_Status='TRUE'*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan 'Status' kısmı 'True' olanları listelettik.*/
+
+/*SELECT * FROM tblPersonal WHERE pers_Name LIKE '%M'*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan adı 'M' ile biten verilerimizi listelettik.*/
