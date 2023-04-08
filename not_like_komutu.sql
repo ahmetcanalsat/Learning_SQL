54eba2de460e5776e868ae0b519facc3de25e67a
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-09 00:47:36 +0300

    Not_Like_komutu_kullanımı
    not like komutuna bir kaç örnek

---
+++ not_like_komutu.sql
@@ -0,0 +1,13 @@
+﻿use DbUrunYonetim
+
+/*SELECT * FROM tblPersonal WHERE pers_City NOT LIKE 'B%'*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan 'City' sütunu 'B' ile başlamayan verilerimizi listelettik.*/
+
+/*SELECT * FROM tblPersonal WHERE pers_Status=0*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan 'Status' sütunu 0(FALSE) olanları listelettik.*/
+
+/*SELECT * FROM tblPersonal WHERE pers_Name NOT LIKE '%T%'*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan Name sütununda içinde 'T' olmayan verileri listelettik.*/
+
+/*SELECT * FROM tblPersonal WHERE pers_Name NOT LIKE '%E%'*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan Name sütununda içinde 'E' olmayan verileri listelettik.*/
