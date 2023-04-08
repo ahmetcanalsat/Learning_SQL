0daf15047756a0e040b4da748631051b0f9c3e28
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-09 01:23:40 +0300

    IN_KOMUTU Örnekler
    IN komutunun nasıl kullanılacağı ve ne işe yaradığını öğrendik

---
+++ In_komutu.sql
@@ -0,0 +1,7 @@
+﻿use DbUrunYonetim
+
+/*SELECT * FROM tblPersonal WHERE pers_City IN('ANKARA','BALIKESİR')*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan Şehir sütununda 'Ankara' veya 'Balıkesir' yazanları listelettik.*/
+
+/*SELECT * FROM tblPersonal WHERE pers_Name LIKE '%A%' OR pers_City IN('BURSA','iSTANBUL')*/
+/*Yukarıdaki sorgumuz ile Personal tablomuzda Adının içinde 'A' harfi bulunan veya Şehri 'Bursa' veya 'İstanbul' olanları listelettik.*/
