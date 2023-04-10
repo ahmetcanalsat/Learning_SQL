1be814a1c3c2a3884ca62dd5fc80435bf157b1de
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-10 23:37:36 +0300

    not_in_kullanımı
    not in komutumuz ne işe yarar ve nasıl kullanılır, öğrendik

---
+++ NOT_IN_komutu.sql
@@ -0,0 +1,8 @@
+﻿use DbUrunYonetim
+
+/*SELECT * FROM tblPersonal WHERE pers_City NOT IN('Ankara','İstanbul','Trabzon')*/
+/*Yukarıdaki komutumuzla Personal tablomuzdan Şehir sütunu "Ankara,İstanbul ve Trabzon" olmayanları listelettik.*/
+
+/*UPDATE tblPersonal SET pers_Salary+=pers_Salary*20/100 WHERE pers_City NOT IN('ANKARA','İSTANBUL')*/
+/*Yukarıdaki komutumuzla Personal tablomuzdaki verilerden Ankara ve İstanbulda yaşamayanların maaşlarına %20 zam yaptırdık.*/
+
