9ca37e771640f51cbfc72d6027db5e8cbde12477
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-13 04:18:32 +0300

    FunctionOfCount
    Count fonksiyonu nedir ve nasıl kullanılır öğrendik.

---
+++ count_fonksiyonu.sql
@@ -0,0 +1,12 @@
+﻿use DbUrunYonetim
+
+/*COUNT fonksiyonu bir tabloda istenen nitelikteki değerin kaç adet olduğunu yazdırır.*/
+
+
+/*SELECT COUNT(*) FROM tblProduct*/ 
+/*Yukarıdaki sorgumuz ürün tablomuzda kaç ürün olduğunu gösterir.*/
+
+/*SELECT COUNT(*) FROM tblProduct WHERE prod_Name='Buzdolabı' */
+/*Yukarıdaki sorgumuz ile ürün tablomuzda kaç adet 'Buzdolabı' isminde ürün olduğunu yazdırdık.*/
+
+SELECT COUNT(*) FROM tblProduct WHERE prod_Stock>50 AND prod_sellPrice<1000
