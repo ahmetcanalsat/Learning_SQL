def3e0fdc9e370a29816a2ca9f37b9e0489a8e19
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-14 20:41:17 +0300

    Sum_fonksiyonu
    Sum fonksiyonu nasıl ve nerelerde kullanılır örneklerler öğrendik.

---
+++ sum_fonksiyonu.sql
@@ -0,0 +1,10 @@
+﻿USE DbUrunYonetim
+
+/*SELECT SUM(prod_Stock) FROM tblProduct*/
+/*Yukarıdaki sorgumuzda SUM komutumuz ile PRODUCT tablomuzdaki ürünlerin STOK değerlerinin toplamını yazdırdık.*/
+
+/*SELECT SUM(prod_Stock) FROM tblProduct WHERE prod_Name='Laptop'*/
+/*Yukarıdaki sorgumuzda PRODUCT tablomuzdaki LAPTOP isimli verilerin STOK değerlerinin toplamını yazdırdık.*/
+
+/*SELECT SUM(prod_sellPrice) FROM tblProduct*/
+/*Yukarıdaki sorgumuzla PRODUCT tablomuzdaki tüm ürünlerin SATIŞ FİYATINDAN satıldığında ne kadar para getireceğini hesapladık.*/
