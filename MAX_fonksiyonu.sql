bdada9392d40f64d12786880fe2cca764b19b024
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-15 11:30:18 +0300

    max_fonksiyonu
    Örneklerle ve açıklamalarla max fonksiyonumuzu nasıl ve nerelerde kullanacağımız öğrendik.

---
+++ MAX_fonksiyonu.sql
@@ -0,0 +1,12 @@
+﻿USE DbUrunYonetim
+
+/*MAX FONKSİYONU: Bir tabloda bulunan aritmetik değerler içerisinde ilgili sütuna ait en yüksek değeri veren fonksiyondur.*/
+
+/*SELECT MAX(prod_Stock) FROM tblProduct*/
+/*Yukarıdaki komutumuzla PRODUCT tablomuzdaki STOCK sütunu en büyük olan değeri yazdırır.*/
+
+/*SELECT MAX(prod_sellPrice) FROM tblProduct*/
+/*Yukarıdaki komutumuzla PRODUCT tablomuzdaki SATIŞ FİYATI EN BÜYÜK olan değeri yazdırdık.*/
+
+/*SELECT MAX(PROD_SELLPRICE) FROM tblProduct WHERE prod_Name='LAPTOP'*/
+/*Yukarıdaki komutumuzla PRODUCT tablomuzdaki SATIŞ FİYATI EN BÜYÜK olan laptop'un fiyatını yazdırdık.*/
