067033f9e2259103175c25abca0f2c6bc24426de
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-15 11:40:43 +0300

    min_fonksiyonu

--- min_fonksiyonu.sql
+++ min_fonksiyonu.sql
@@ -2,11 +2,11 @@
 
 /*MIN Fonksiyonu: Bir tablodaki aritmetik değerler içerisinde ilgili sütuna ait en düşük değeri veren fonksiyondur.*/
 
-SELECT MIN(prod_Stock) FROM tblProduct
+/*SELECT MIN(prod_Stock) FROM tblProduct*/
 /*Yukarıdaki komutumuzla PRODUCT tablomuzdaki STOCK sütunu en küçük olan değeri yazdırır.*/
 
-SELECT MIN(prod_sellPrice) FROM tblProduct
+/*SELECT MIN(prod_sellPrice) FROM tblProduct*/
 /*Yukarıdaki komutumuzla PRODUCT tablomuzdaki SATIŞ FİYATI en küçük olan değeri yazdırdık.*/
 
-SELECT MIN(PROD_SELLPRICE) FROM tblProduct WHERE prod_Name='BUZDOLABI'
+/*SELECT MIN(PROD_SELLPRICE) FROM tblProduct WHERE prod_Name='BUZDOLABI'*/
 /*Yukarıdaki komutumuzla PRODUCT tablomuzdaki SATIŞ FİYATI en küçük olan buzdolabının değerini yazdırdık.*/
