d39b54002d502dd97fd7bb46ab08eb53ef51d3ad
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-15 11:35:01 +0300

    min_fonksiyonu
    Örneklerle ve açıklamalarla min_fonksiyonumuzu nasıl ve nerelerde kullanacağımız öğrendik

---
+++ min_fonksiyonu.sql
@@ -0,0 +1,12 @@
+﻿USE DbUrunYonetim
+
+/*MIN Fonksiyonu: Bir tablodaki aritmetik değerler içerisinde ilgili sütuna ait en düşük değeri veren fonksiyondur.*/
+
+SELECT MIN(prod_Stock) FROM tblProduct
+/*Yukarıdaki komutumuzla PRODUCT tablomuzdaki STOCK sütunu en küçük olan değeri yazdırır.*/
+
+SELECT MIN(prod_sellPrice) FROM tblProduct
+/*Yukarıdaki komutumuzla PRODUCT tablomuzdaki SATIŞ FİYATI en küçük olan değeri yazdırdık.*/
+
+SELECT MIN(PROD_SELLPRICE) FROM tblProduct WHERE prod_Name='BUZDOLABI'
+/*Yukarıdaki komutumuzla PRODUCT tablomuzdaki SATIŞ FİYATI en küçük olan buzdolabının değerini yazdırdık.*/
