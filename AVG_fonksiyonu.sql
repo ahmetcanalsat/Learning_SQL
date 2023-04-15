e479d4c18d6b090f89f978b3295f5a40629421dc
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-15 10:35:31 +0300

    AVG_fonksiyonu
    Örneklerle AVG fonksiyonun ne işe yaradığını ve nasıl kullanabileceğimizi öğrendik.

---
+++ AVG_fonksiyonu.sql
@@ -0,0 +1,9 @@
+﻿USE DbUrunYonetim
+
+/*Belirttiğimiz sütunların ORTALAMASINI veren komuttur.*/
+
+/*SELECT AVG(prod_Stock) FROM tblProduct*/
+/*Yukarıdaki sorgumuzda PRODUCT tablomuzdaki değerlere göre STOK sütunumuzun ORTALAMASINI buluyoruz.*/
+
+/*SELECT AVG(prod_sellPrice) FROM tblProduct*/
+/*Yukarıdaki sorgumuz ile PRODUCT tablomuzdaki ürünlerin SATIŞ FİYATININ ORTALAMASINI bulduk.*/
