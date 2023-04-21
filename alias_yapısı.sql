5731e2ee9e48a20c41d84eb21f76b565fbabc8ff
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-21 08:28:25 +0300

    alias_yapısı
    Açıklama ve örneklerle AS komutunun nasıl ve nerelerde kullanacağını öğrenmiş olduk.

---
+++ alias_yapısı.sql
@@ -0,0 +1,9 @@
+﻿USE DbUrunYonetim
+
+/*Sorgularımızda var olan sütunun yerine geçici bir isim koymamızı sağlar. TABLODAKİ_SÜTUN_ADI AS BİZİM_KOYACAĞIMIZ_SÜTUN_ADI şeklinde kullanımı mevcuttur.*/
+
+/*SELECT prod_Name,prod_Code AS 'ÜRÜN KODU', prod_Stock AS 'TOPLAM STOK' FROM tblProduct*/
+/*Yukarıdaki sorgumuzda PRODUCT tablomuzdaki isim,kod ve stok değerlerini çağırdık fakat KOD ve STOK sütunlarına yeni bir isim atadık.*/
+
+/*SELECT PROD_NAME AS 'İSİM', PROD_SELLPRICE AS 'SATIŞ FİYATI' FROM tblProduct*/
+/*Yukarıdaki sorgumuzda PRODUCT tablomuzdaki isim ve satış fiyatı sütunlarını yeniden adlandırıp listesini oluşturuyoruz.*/
