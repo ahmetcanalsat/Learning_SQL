0b48a8562a852a2c6b74d2fcae333e41a5bf1c07
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-26 16:14:47 +0300

    null_deger_yonetimi
    Null değerleri nasıl yönetebileceğimizi açıklama ve örneklerle gösterdik.

---
+++ null_komutu.sql
@@ -0,0 +1,13 @@
+﻿USE DbUrunYonetim
+
+/*NULL değerlerle işlem yapabilmek için 'IS NULL' ifadesini kullanırız.*/
+
+
+/*SELECT * FROM tblPersonal WHERE pers_City IS NULL*/
+/*Yukarıdaki komutumuzla PERSONAL tablomuzdaki ŞEHİR sütunu NULL olanları listelettik.*/
+
+/*UPDATE tblPersonal SET pers_Department='GÜNCELLENECEK' WHERE pers_Department IS NULL*/
+/*Yukarıdaki sorgumuzla PERSONAL tablomuzdaki DEPARTMAN değeri NULL olan sütunları GÜNCELLENECEK olarak değiştirdik.*/
+
+/*SELECT * FROM tblPersonal WHERE pers_City IS NOT NULL*/
+/*Yukarıdaki sorgumuzla PERSONAL tablomuzdaki ŞEHİR sütunu NULL OLMAYANLARI listelettik.*/
