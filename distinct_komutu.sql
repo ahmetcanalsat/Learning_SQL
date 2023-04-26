c3f72b51091dc325ca27f1df89732eabd5d9e8bb
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-04-26 20:15:09 +0300

    distinct_kullanimi
    DISTINCT metodu nedir ve nasıl kullanılır açıkmalar ve örneklerle gösterdik.

---
+++ distinct_komutu.sql
@@ -0,0 +1,12 @@
+﻿USE DbUrunYonetim
+
+/*DISTINCT METODU : Listelenecek verilerin tekrarsız şekilde listelenmesini sağlar.*/
+
+/*SELECT customer_City FROM tblCustomer
+/*Yukarıdaki sorgumuzla CUSTOMER tablomuzdaki şehirleri listeledik.*/
+
+SELECT DISTINCT(customer_City) FROM tblCustomer*/
+/*Yukarıdaki sorgumuzla CUSTOMER tablomuzdaki şehirleri DISTINCT komutu kullanarak tekrarsız listeledik.*/
+
+/*SELECT DISTINCT(pers_Department) FROM tblPersonal WHERE pers_City='Ankara'*/
+/*Yukarıdaki sorgumuzla PERSONAL tablomuzdaki ŞEHRİ Ankara olan çalışanların DEPARTMANLARINI listelettik.*/
